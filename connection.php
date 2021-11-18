ab3d528b04cdbd9c5c574f15deddba5a237e3fd0
artAluno <93623192+artAluno@users.noreply.github.com>
2021-11-18 19:59:15 -0300

    update01-18/11

--- connection.php
+++ connection.php
@@ -1,5 +1,5 @@
 <?php
-    $mysqli = new mysqli("sql10.freesqldatabase.com", "sql10448585", "QXQDRT7Rmg", "sql10448585");
+    $mysqli = new mysqli("sql10.freesqldatabase.com", "sql10452085", "kMpLSI4XEA", "sql10452085");
     if (mysqli_connect_errno()) {
         printf("Connect failed: %s\n", mysqli_connect_error());
         exit();
