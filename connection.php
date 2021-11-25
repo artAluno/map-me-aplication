6ba5cc3a18487859339b1af7f5348a15fae6d3cb
artAluno <93623192+artAluno@users.noreply.github.com>
2021-11-25 14:33:09 -0300

    update-newDB

--- connection.php
+++ connection.php
@@ -1,5 +1,5 @@
 <?php
-    $mysqli = new mysqli("sql10.freesqldatabase.com", "sql10452085", "kMpLSI4XEA", "sql10452085");
+    $mysqli = new mysqli("remotemysql.com", "LcytHOt4Cg", "ictZxbgx4b", "LcytHOt4Cg");
     if (mysqli_connect_errno()) {
         printf("Connect failed: %s\n", mysqli_connect_error());
         exit();
